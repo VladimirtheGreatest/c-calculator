f96d2a4c7d8f557b7b15576ee75bdf1d91baffdf
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-12-14 20:53:12 +0000

    Converted calculator to OOP using classes and objects.

---
+++ ConsoleApp1/CalCulator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class CalCulator
+    {
+        // Add two integers and returns the sum  
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        // Multiply two integers and retuns the result  
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        // Subtracts small number from big number  
+        public int Subtract(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1 - num2;
+            }
+
+            return num2 - num1;
+
+        }
+        //performing Division on two float variables.  
+        public float Division(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+    }
+}

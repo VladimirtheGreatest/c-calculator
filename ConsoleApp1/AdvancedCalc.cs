51053c6b166ec1305f12de32626e89d9361423d7
VladimirtheGreatest <vladkodoktorov@gmail.com>
2019-12-17 18:12:15 +0000

    Added square math option.

--- ConsoleApp1/AdvancedCalc.cs
+++ ConsoleApp1/AdvancedCalc.cs
@@ -6,14 +6,6 @@ namespace ConsoleApp1
 {
     class AdvancedCalc : CalCulator
     {
-        public double number1;
-
-        //Note that the constructor name must match the class name, and it cannot have a return type (like void or int).
-        //Also note that the constructor is called when the object is created.
-        public AdvancedCalc(double aNumber1)
-        {
-            aNumber1 = number1;
-        }
         public double Square(double number1)
         {
             return Math.Sqrt(number1);
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,12 +13,6 @@ namespace ConsoleApp1
             {
                 bool IsTrue = true;
                 Console.WriteLine("Welcome to better calculator 1.2");
-                Console.WriteLine(CalCulator.Test);
-                AdvancedCalc calculateme = new AdvancedCalc(81);
-                calculateme.number1 = 9;
-                Console.WriteLine(calculateme.number1);
-                Console.WriteLine(calculateme.Square(81));
-                Console.WriteLine(calculateme.Add(81, 81));
                 while (IsTrue == true)
                 {
                     //implement option to calculate more than 2 number like for example 5+5+5+5
@@ -27,14 +21,6 @@ namespace ConsoleApp1
                         Console.WriteLine("Your current time is " + DateTime.Now);
 
                         Calculation();
-                        Console.Write("Press 'y' to continue, press anything else to close this app:");
-                        string proceed = Console.ReadLine();
-                        if (proceed != "y")
-                        {
-                            Console.WriteLine("Thanks for using this calculator");
-                            Environment.Exit(0);
-                        }
-
                     }
                     catch (Exception e)
                     {
@@ -49,32 +35,40 @@ namespace ConsoleApp1
 
                 Console.Write("Enter the operator: ");
                 string Operator = Console.ReadLine();
-
-                Console.Write("Enter your second number: ");
-                double number2 = Convert.ToDouble(Console.ReadLine());
-
-
-                CalCulator calculate = new CalCulator();
-
-                if (Operator == "+")
+                if (Operator == "sq")
                 {
-                    Console.WriteLine(calculate.Add(number1, number2));
-                }
-                else if (Operator == "-")
-                {
-                    Console.WriteLine(calculate.Subtract(number1, number2));
-                }
-                else if (Operator == "*")
-                {
-                    Console.WriteLine(calculate.Multiply(number1, number2));
-                }
-                else if (Operator == "/")
-                {
-                    Console.WriteLine(calculate.Division(number1, number2));
+                    AdvancedCalc calculateme = new AdvancedCalc();
+                    Console.WriteLine(calculateme.Square(number1));
                 }
                 else
                 {
-                    Console.WriteLine("Invalid operator");
+
+                    Console.Write("Enter your second number: ");
+                    double number2 = Convert.ToDouble(Console.ReadLine());
+
+
+                    CalCulator calculate = new CalCulator();
+
+                    if (Operator == "+")
+                    {
+                        Console.WriteLine(calculate.Add(number1, number2));
+                    }
+                    else if (Operator == "-")
+                    {
+                        Console.WriteLine(calculate.Subtract(number1, number2));
+                    }
+                    else if (Operator == "*")
+                    {
+                        Console.WriteLine(calculate.Multiply(number1, number2));
+                    }
+                    else if (Operator == "/")
+                    {
+                        Console.WriteLine(calculate.Division(number1, number2));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid operator");
+                    }
                 }
             }
         }
